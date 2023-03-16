a6413527163e926b62cd9cdf96e67bb7d7ab11ae
Manu368 <116960480+Manu368@users.noreply.github.com>
2023-03-16 18:38:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,114 @@
+let imageFirst = document.getElementById("image01");
+
+
+let imag = document.createElement("img");
+imag.classList.add("img01");
+imag.src = "https://res.cloudinary.com/dqrttxm5s/image/upload/v1676833102/08.image_mfxnmh.webp";
+imageFirst.appendChild(imag);
+
+
+
+let productlist = [{
+       a:  "https://res.cloudinary.com/dqrttxm5s/image/upload/v1676833102/08.image_mfxnmh.webp",
+       b: 1
+    },
+    {
+        a: "https://res.cloudinary.com/dqrttxm5s/image/upload/v1678972005/01st_yurred.webp",
+        b: 2
+    },
+    {
+        a: "https://res.cloudinary.com/dqrttxm5s/image/upload/v1678972005/02nd_kz8w4e.webp", 
+        b: 3
+    },
+
+    
+
+    
+   
+   
+];
+
+
+
+let textElement = document.getElementById("text01")
+
+
+
+let headingElement = document.createElement("h1");
+headingElement.classList.add("heading01");
+headingElement.textContent = "Unisex Black Galaxy Fit Fitness Band";
+textElement.appendChild(headingElement);
+
+
+let headingElemnt01 = document.createElement("h4");
+headingElemnt01.classList.add("heading02");
+headingElemnt01.textContent = "samsung";
+textElement.appendChild(headingElemnt01);
+
+
+let headingElement02 = document.createElement("h3");
+headingElement02.classList.add("heading03");
+headingElement02.textContent = "Price: Rs";
+textElement.appendChild(headingElement02);
+
+
+let spanElement = document.createElement("span");
+spanElement.classList.add("span01");
+spanElement.textContent = "9990";
+headingElement02.appendChild(spanElement);
+
+
+let headingElement04 = document.createElement("h3");
+headingElement04.classList.add("heading03");
+headingElement04.textContent = "Description";
+textElement.appendChild(headingElement04);
+
+
+let paraElement = document.createElement("p");
+paraElement.classList.add("para01")
+paraElement.textContent = "Keep up your routine for even longer. With plenty of power to last throughout your daily workouts, you can pack in those longer hours to reach your fitness goals. What's more, you won't have to worry about disruptive battery discharge when tracking your activity.";
+textElement.appendChild(paraElement);
+
+
+let headingElement05 = document.createElement("h3");
+headingElement05.classList.add("heading03");
+headingElement05.textContent = "Product Preview";
+textElement.appendChild(headingElement05);
+
+
+let Element001 = document.getElementById("java01");
+
+
+
+
+function sachin(kohli){
+let smallimg = document.createElement("img");
+smallimg.classList.add("small01");
+smallimg.src = kohli.a;
+smallimg.id = kohli.b;
+textElement.appendChild(smallimg)
+
+
+smallimg.onclick = function(){
+    imag.src = smallimg.src;
+    smallimg.style.border = "3px solid #009688"
+}
+smallimg.onmouseout = function(){
+    imag.src = smallimg.src;
+    smallimg.style.border = "";
+}
+
+}
+
+
+
+for(let kohli of productlist){
+    sachin(kohli)
+}
+
+
+
+let btnElement = document.createElement("button");
+btnElement.classList.add("button01");
+btnElement.textContent = "Add to Cart";
+textElement.appendChild(btnElement);
